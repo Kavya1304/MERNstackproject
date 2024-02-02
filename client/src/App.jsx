020a50680460b7b6297b7f4b32e9bd78991ffc40
Padala Kavya <kavyapadala890@gmail.com>
2024-02-02 17:03:14 +0530

    complete sign up page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import {BrowserRouter, Routes, Route  } from "react-router-dom";
-import SignIn from "./pages/Signin";
-import SignUp from "./pages/SignUp";
-import About from "./pages/About";
-import Profile from "./pages/Profile";
-import Home from "./pages/Home";
-import Header from "./components/Header";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import About from "./pages/About.jsx";
+import Profile from "./pages/Profile.jsx";
+import Home from "./pages/Home.jsx";
+import Header from "./components/Header.jsx";
 
 export default function App() {
   return (
