6be946fa72f8cf1ab9430cb3de24ead3fadf53c8
Padala Kavya <kavyapadala890@gmail.com>
2024-02-07 13:54:51 +0530

    Complete lisitng page and contact landlord functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -10,4 +10,6 @@ router.post('/create', verifyToken ,createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
 router.post('/update/:id', verifyToken, updateListing);
 router.get('/get/:id', getListing);
+
+
 export default router;
